53dfb472261781a7b77e2540792bd8f09c8fb807
Yeasin <realalyeasin@gmail.com>
2022-05-25 14:32:17 +0600

    search implemented partially

--- lib/Controller/todo_controller.dart
+++ lib/Controller/todo_controller.dart
@@ -2,36 +2,39 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:todo_app/Model/todo_model.dart';
 
-class ToDoController extends GetxController{
+class ToDoController extends GetxController {
   var isLoading = false;
   var todoList = <ToDoModel>[];
-  var searchList = List.empty(growable: true).obs;
-  Future<void> addToD(String task, bool done, String id) async{
-    await FirebaseFirestore.instance.collection('todos').doc((id != '' ? id : null)).set({
-      'task' : task,
-      'isDone' : done
-    },SetOptions(merge: true)).then((value) => Get.back());
+  var searchList = [].obs;
+
+  Future<void> addToD(String task, bool done, String id) async {
+    await FirebaseFirestore.instance.collection('todos').doc(
+        (id != '' ? id : null)).set({
+      'task': task,
+      'isDone': done
+    }, SetOptions(merge: true)).then((value) => Get.back());
   }
 
   Future<void> getData() async {
-    try{
-      QuerySnapshot _taskSnap = await FirebaseFirestore.instance.collection('todos').orderBy('task').get();
+    try {
+      QuerySnapshot _taskSnap = await FirebaseFirestore.instance.collection(
+          'todos').orderBy('task').get();
       todoList.clear();
-      for(var item in _taskSnap.docs){
-        todoList.add(ToDoModel(item['task'], item['isDone'],item.id));
+      for (var item in _taskSnap.docs) {
+        todoList.add(ToDoModel(item['task'], item['isDone'], item.id));
       }
-      isLoading=false;
+      isLoading = false;
       update();
-    } catch(e){
+    } catch (e) {
       Get.snackbar("Error", "${e.toString()}");
     }
   }
-  void delete(String id){
+
+  void delete(String id) {
     FirebaseFirestore.instance.collection('todos').doc(id).delete();
   }
 
-  getSearch(String q)async{
-    (q != '' && q != null) ? searchList = (await FirebaseFirestore.instance.collection('todos').where('task',arrayContains: q).snapshots()) as RxList :
-   searchList =(await FirebaseFirestore.instance.collection('todos').snapshots()) as RxList;
+  Future queryData(String q) async {
+    return FirebaseFirestore.instance.collection('todos').where('task',isGreaterThanOrEqualTo: q).get();
   }
 }
--- lib/Screens/search_notes.dart
+++ lib/Screens/search_notes.dart
@@ -3,51 +3,71 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-
 import '../Controller/todo_controller.dart';
 
 class SearchNotes extends StatelessWidget {
   SearchNotes({Key? key}) : super(key: key);
   ToDoController todo = Get.put(ToDoController());
+  TextEditingController textEditingController = TextEditingController();
+  QuerySnapshot? snapshot;
+  RxBool ex = false.obs;
   @override
   Widget build(BuildContext context) {
-    String q = '';
-    return Obx(()=>Scaffold(
-        backgroundColor: Color.fromRGBO(232, 245, 182, 1),
-        appBar: AppBar(
+    return Obx(() => Scaffold(
+          backgroundColor: Color.fromRGBO(232, 245, 182, 1),
+          appBar: AppBar(
             toolbarHeight: 70,
             backgroundColor: Color.fromRGBO(54, 115, 125, 1),
             title: Card(
               child: TextField(
                 decoration: InputDecoration(
-                  prefixIcon: Icon(FontAwesomeIcons.search),
                   hintText: 'Search To-Do',
                   border: InputBorder.none,
                   floatingLabelAlignment: FloatingLabelAlignment.center,
                 ),
-                onChanged: (value){
-                  q=value;
-                  todo.getSearch(q);
-                },),
-            )),
-        body: ListView.builder(
-            itemCount: todo.searchList.value.length,
-            itemBuilder: (BuildContext,index){
-              return Padding(
-                padding: const EdgeInsets.only(
-                    left: 10, right: 10, top: 10),
-                child: Row(
-                  children: [
-                    Text('${todo.searchList.value[index]['task']}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18),
-                    ),
-                  ],
-                ),
-              );
-            })
-
-    ));
+                onChanged: (value) {},
+              ),
+            ),
+            actions: [
+              GetBuilder<ToDoController>(
+                  init: ToDoController(),
+                  builder: (val) {
+                    return IconButton(
+                        onPressed: () {
+                          val
+                              .queryData(textEditingController.text)
+                              .then((value) {
+                            snapshot = value;
+                            ex.toggle();
+                          });
+                        },
+                        icon: Icon(Icons.search));
+                  })
+            ],
+          ),
+          body: ex.value
+              ? ListView.builder(
+                  itemCount: snapshot!.docs.length,
+                  itemBuilder: (BuildContext context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+                      child: Text(
+                        '${snapshot!.docs[index]['task']}',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  })
+              : Container(),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.black,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            onPressed: () {
+              ex.value = false;
+            },
+            child: Icon(FontAwesomeIcons.trashCanArrowUp),
+          ),
+        ));
   }
 }
